02a01d34fae658bed4db8724ccc9a74fad4b54da
samuelribeiroo <samuelribeirodev@gmail.com>
2024-08-08 16:22:46 -0300

    update: new data stored at json

--- src/database/api.js
+++ src/database/api.js
@@ -1,26 +1,195 @@
 export const GameListAPI = [
-    {
-        id: 1,
-        imageURL: "https://assetsio.gnwcdn.com/eurogamer-zjp1vx.jpg?width=1200&height=600&fit=crop&enable=upscale&auto=webp",
-        name: "GTA V",
-        description: "GTA V é um jogo de ação e aventura da Rockstar, ambientado em um mundo aberto cheio de missões emocionantes.",
-    },
-    {
-        id: 2,
-        imageURL: "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/elden.avif?alt=media&token=15292911-8445-4112-bb5f-8f2e52bf0435",
-        name: "Elden Ring",
-        description: "Elden Ring é um RPG de ação da FromSoftware, apresentando um vasto mundo de fantasia repleto de desafios e mistérios.",
-    },
-    {
-        id: 3,
-        imageURL: "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/cod.avif?alt=media&token=43f7d53b-b560-4c57-8468-412983adaf27",
-        name: "Call of Duty",
-        description: "Call of Duty é uma série de jogos de tiro em primeira pessoa, conhecida por suas intensas campanhas militares e multiplayer competitivo.",
-    },
-    {
-        id: 4,
-        imageURL: "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/fortnite.avif?alt=media&token=eca1ea0b-f0c9-4911-b326-fc652092fe4c",
-        name: "Fortnite",
-        description: "Fortnite é um jogo de sobrevivência e construção da Epic Games, famoso por seu modo Battle Royale de ritmo rápido.",
-    },
+  {
+    id: 1,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/gta-v.webp?alt=media&token=0532b94f-d1b6-42c6-b6f2-7f2b1d3fc746",
+    name: "GRAND THEFT AUTO V",
+    description:
+      "GTA V é um jogo de ação e aventura da Rockstar, ambientado em um mundo aberto cheio de missões emocionantes.",
+  },
+  {
+    id: 2,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/nba-2k25.avif?alt=media&token=b710794e-3174-4166-b591-1300e09bdf51",
+    name: "NBA 2K25",
+    description:
+      "Elden Ring é um RPG de ação da FromSoftware, apresentando um vasto mundo de fantasia repleto de desafios e mistérios.",
+  },
+  {
+    id: 3,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/firt.jpg?alt=media&token=beb73103-8977-4f4c-bbf2-f64cbc85a33e",
+    name: "DIRT 5",
+    description:
+      "Call of Duty é uma série de jogos de tiro em primeira pessoa, conhecida por suas intensas campanhas militares e multiplayer competitivo.",
+  },
+  {
+    id: 4,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/witcher.avif?alt=media&token=4004281d-6546-46bc-90bb-a912bfaee419",
+    name: "THE WITCHER",
+    description:
+      "Fortnite é um jogo de sobrevivência e construção da Epic Games, famoso por seu modo Battle Royale de ritmo rápido.",
+  },
+  {
+    id: 5,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/college-footbal.avif?alt=media&token=6a01ca84-9b89-445c-bf30-34933150bae1",
+    name: "COLLEGE FOOTBAL 25",
+    description:
+      "GTA V é um jogo de ação e aventura da Rockstar, ambientado em um mundo aberto cheio de missões emocionantes.",
+  },
+  {
+    id: 6,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/spider.webp?alt=media&token=e6931d23-b9b8-4b82-8885-949ede9a99f3",
+    name: "SPIDER MAN MILES MORALES",
+    description:
+      "Elden Ring é um RPG de ação da FromSoftware, apresentando um vasto mundo de fantasia repleto de desafios e mistérios.",
+  },
+  {
+    id: 7,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/black-ops.avif?alt=media&token=7d52a59f-df30-46d4-932b-55fd56dc4705",
+    name: "CALL OF DUTY - COLD WAR",
+    description:
+      "Call of Duty é uma série de jogos de tiro em primeira pessoa, conhecida por suas intensas campanhas militares e multiplayer competitivo.",
+  },
+  {
+    id: 8,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/cod-modern.webp?alt=media&token=7b510abc-d3e0-4c60-9c3d-fb1a540751b8",
+    name: "CALL OF DUTY - MODERN WARFARE",
+    description:
+      "Fortnite é um jogo de sobrevivência e construção da Epic Games, famoso por seu modo Battle Royale de ritmo rápido.",
+  },
+  {
+    id: 9,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/dark-side.avif?alt=media&token=4fc04993-b63f-418a-8c72-f477a6e74a3e",
+    name: "DARKSIDERS GENESIS",
+    description:
+      "GTA V é um jogo de ação e aventura da Rockstar, ambientado em um mundo aberto cheio de missões emocionantes.",
+  },
+  
+  // {
+  //   id: 10,
+  //   imageURL:
+  //     "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/battfle4.jpg?alt=media&token=1e3d2e77-ee2d-441d-ba57-307d172de86d",
+  //   name: "Elden Ring",
+  //   description:
+  //     "Elden Ring é um RPG de ação da FromSoftware, apresentando um vasto mundo de fantasia repleto de desafios e mistérios.",
+  // },
+  // {
+  //   id: 11,
+  //   imageURL:
+  //     "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/cod.avif?alt=media&token=43f7d53b-b560-4c57-8468-412983adaf27",
+  //   name: "Call of Duty",
+  //   description:
+  //     "Call of Duty é uma série de jogos de tiro em primeira pessoa, conhecida por suas intensas campanhas militares e multiplayer competitivo.",
+  // },
+  // {
+  //   id: 12,
+  //   imageURL:
+  //     "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/doom.webp?alt=media&token=310cbee5-ccd8-4d5b-89d7-40891a81dae5",
+  //   name: "Fortnite",
+  //   description:
+  //     "Fortnite é um jogo de sobrevivência e construção da Epic Games, famoso por seu modo Battle Royale de ritmo rápido.",
+  // },
+  // {
+  //   id: 5,
+  //   imageURL:
+  //     "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/neon-blood.avif?alt=media&token=ea6dd3ef-e113-47e3-9c27-5c67a496a4af",
+  //   name: "GTA V",
+  //   description:
+  //     "GTA V é um jogo de ação e aventura da Rockstar, ambientado em um mundo aberto cheio de missões emocionantes.",
+  // },
+  // {
+  //   id: 6,
+  //   imageURL:
+  //     "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/new-worl.avif?alt=media&token=2017a417-74e6-4fec-9baf-8561bc2ee8d3",
+  //   name: "Elden Ring",
+  //   description:
+  //     "Elden Ring é um RPG de ação da FromSoftware, apresentando um vasto mundo de fantasia repleto de desafios e mistérios.",
+  // },
+  // {
+  //   id: 7,
+  //   imageURL:
+  //     "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/dbz.avif?alt=media&token=09009c0b-030c-4d9f-94bf-799c7b2557bc",
+  //   name: "Call of Duty",
+  //   description:
+  //     "Call of Duty é uma série de jogos de tiro em primeira pessoa, conhecida por suas intensas campanhas militares e multiplayer competitivo.",
+  // },
+  // {
+  //   id: 8,
+  //   imageURL:
+  //     "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/sonic.avif?alt=media&token=991e77b4-1cc4-493b-b0ec-5b7f2200d06a",
+  //   name: "Fortnite",
+  //   description:
+  //     "Fortnite é um jogo de sobrevivência e construção da Epic Games, famoso por seu modo Battle Royale de ritmo rápido.",
+  // },
+  // {
+  //   id: 1,
+  //   imageURL:
+  //     "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/gta-v.webp?alt=media&token=0532b94f-d1b6-42c6-b6f2-7f2b1d3fc746",
+  //   name: "GTA V",
+  //   description:
+  //     "GTA V é um jogo de ação e aventura da Rockstar, ambientado em um mundo aberto cheio de missões emocionantes.",
+  // },
+  // {
+  //   id: 2,
+  //   imageURL:
+  //     "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/elden.avif?alt=media&token=15292911-8445-4112-bb5f-8f2e52bf0435",
+  //   name: "Elden Ring",
+  //   description:
+  //     "Elden Ring é um RPG de ação da FromSoftware, apresentando um vasto mundo de fantasia repleto de desafios e mistérios.",
+  // },
+  // {
+  //   id: 3,
+  //   imageURL:
+  //     "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/cod.avif?alt=media&token=43f7d53b-b560-4c57-8468-412983adaf27",
+  //   name: "Call of Duty",
+  //   description:
+  //     "Call of Duty é uma série de jogos de tiro em primeira pessoa, conhecida por suas intensas campanhas militares e multiplayer competitivo.",
+  // },
+  // {
+  //   id: 4,
+  //   imageURL:
+  //     "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/fortnite.avif?alt=media&token=eca1ea0b-f0c9-4911-b326-fc652092fe4c",
+  //   name: "Fortnite",
+  //   description:
+  //     "Fortnite é um jogo de sobrevivência e construção da Epic Games, famoso por seu modo Battle Royale de ritmo rápido.",
+  // },
+  // {
+  //   id: 5,
+  //   imageURL:
+  //     "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/neon-blood.avif?alt=media&token=ea6dd3ef-e113-47e3-9c27-5c67a496a4af",
+  //   name: "GTA V",
+  //   description:
+  //     "GTA V é um jogo de ação e aventura da Rockstar, ambientado em um mundo aberto cheio de missões emocionantes.",
+  // },
+  // {
+  //   id: 6,
+  //   imageURL:
+  //     "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/new-worl.avif?alt=media&token=2017a417-74e6-4fec-9baf-8561bc2ee8d3",
+  //   name: "Elden Ring",
+  //   description:
+  //     "Elden Ring é um RPG de ação da FromSoftware, apresentando um vasto mundo de fantasia repleto de desafios e mistérios.",
+  // },
+  // {
+  //   id: 7,
+  //   imageURL:
+  //     "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/dbz.avif?alt=media&token=09009c0b-030c-4d9f-94bf-799c7b2557bc",
+  //   name: "Call of Duty",
+  //   description:
+  //     "Call of Duty é uma série de jogos de tiro em primeira pessoa, conhecida por suas intensas campanhas militares e multiplayer competitivo.",
+  // },
+  // {
+  //   id: 8,
+  //   imageURL:
+  //     "https://firebasestorage.googleapis.com/v0/b/third-project-89e32.appspot.com/o/sonic.avif?alt=media&token=991e77b4-1cc4-493b-b0ec-5b7f2200d06a",
+  //   name: "Fortnite",
+  //   description:
+  //     "Fortnite é um jogo de sobrevivência e construção da Epic Games, famoso por seu modo Battle Royale de ritmo rápido.",
+  // },
 ];
